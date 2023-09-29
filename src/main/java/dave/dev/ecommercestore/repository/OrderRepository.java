68fa8200a85f866545fc27df2b8d824768f2c16c
John David Dela Cruz <johndaviddc1@gmail.com>
2023-09-29 14:24:47 +0800

    added repository

---
+++ src/main/java/dave/dev/ecommercestore/repository/OrderRepository.java
@@ -0,0 +1,12 @@
+package dave.dev.ecommercestore.repository;
+
+import dave.dev.ecommercestore.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
---
+++ src/main/java/dave/dev/ecommercestore/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package dave.dev.ecommercestore.repository;
+
+import dave.dev.ecommercestore.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByName(String name);
+}
