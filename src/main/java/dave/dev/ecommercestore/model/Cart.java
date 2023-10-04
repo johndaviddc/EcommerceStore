3bc25e9666b011abea77ab5f28f8a24078a298bb
John David Dela Cruz <johndaviddc1@gmail.com>
2023-10-04 21:12:20 +0800

    update something

--- src/main/java/dave/dev/ecommercestore/model/Cart.java
+++ src/main/java/dave/dev/ecommercestore/model/Cart.java
@@ -12,11 +12,12 @@ public class Cart {
     private Long id;
 
     @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
-    private List<CartItem> cartItems;
+    private List<CartItem> items;
+    private double totalAmount;
 
     public Cart(Long id, List<CartItem> cartItems) {
         this.id = id;
-        this.cartItems = cartItems;
+        this.items = items;
     }
 
     public Long getId() {
@@ -28,14 +29,37 @@ public class Cart {
     }
 
     public List<CartItem> getCartItems() {
-        return cartItems;
+        return items;
     }
 
     public void setCartItems(List<CartItem> cartItems) {
-        this.cartItems = cartItems;
+        this.items = cartItems;
     }
 
     public Cart() {
 
     }
+    public CartItem findCartItemByProductId(Long productId) {
+        // Implement the logic to find a cart item by product ID
+        for (CartItem item : items) {
+            if (item.getProduct().getId().equals(productId)) {
+                return item; // Found the cart item with the specified product ID
+            }
+        }
+        return null; // Product not found in the cart
+    }
+
+    public void calculateTotal() {
+        double total = 0.0;
+
+        // Calculate the total amount based on the cart items
+        for (CartItem item : items) {
+            total += item.getTotalPrice();
+        }
+
+        // Set the total amount property of the cart
+        this.totalAmount = total;
+    }
+
+
 }
